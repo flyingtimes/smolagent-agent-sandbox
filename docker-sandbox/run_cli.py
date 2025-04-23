3565b52e01b5a32835935bb625bead27ca29e65d
flyingtimes <5200374@qq.com>
2025-04-24 00:19:20 +0800

    refactor(docker-sandbox): 更新代码模板并调整CLI执行顺序
    更新了`default.tmpl`模板，引入了`ToolCallingAgent`和`DuckDuckGoSearchTool`，并调整了`CodeAgent`的配置。同时，修改了`run_cli.py`中的代码执行顺序，优先执行关税新闻查询任务。

--- docker-sandbox/run_cli.py
+++ docker-sandbox/run_cli.py
@@ -7,11 +7,11 @@ sandbox = DockerSandbox()
 # print(result)
 # print("===============================")
 
-# result = sandbox.run_code("""
-# 2025年4月22日中国和美国关于关税都有哪些重大新闻？用中文回答，将结果以markdown格式写入工作目录下的output.md
-# """)
-
 result = sandbox.run_code("""
-mcp工具都有哪些主流的工具，在哪些地方能找到比较全面的mcp工具列表？深度搜索网络信息，给出最常见的20种以上mcp工具，用中文回答，请注明出处，将结果以markdown格式写入工作目录下的output2.md
+2025年4月22日中国和美国关于关税都有哪些重大新闻？用中文回答，将结果以markdown格式写入工作目录下的output.md
 """)
+
+# result = sandbox.run_code("""
+# mcp工具都有哪些主流的工具，在哪些地方能找到比较全面的mcp工具列表？深度搜索网络信息，给出最常见的20种以上mcp工具，用中文回答，请注明出处，将结果以markdown格式写入工作目录下的output2.md
+# """)
 print(result)
